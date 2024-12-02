42886a347799151a7cca8218b0cfca0d9841edba
Hiba <mallickhiba@gmail.com>
2024-12-03 02:58:42 +0500

    last class

---
+++ lib/home_page.dart
@@ -0,0 +1,100 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:iba_course_2/authentication//widgets/sign_in_up_button.dart';
+import 'package:iba_course_2/friends/friends_add.dart';
+import 'package:iba_course_2/friends/friends_services.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Friends'), actions: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: IconButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const AddFriend()));
+              },
+              icon: const Icon(Icons.add)),
+        )
+      ]),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('friends').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No friends added yet.'));
+          }
+          final friends = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: friends.length,
+            itemBuilder: (context, index) {
+              final friend = friends[index].data();
+              return ListTile(
+                title: Text(friend['name']),
+                subtitle: Text(friend['email']),
+                trailing: IconButton(
+                    onPressed: () async {
+                      deleteFriendFromFirestore(
+                          friend['name'], friend['email'], context);
+                    },
+                    icon: const Icon(Icons.delete)),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+void deleteFriendFromFirestore(
+    String name, String email, BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        content: const Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            'Are you sure you want to delete friend?',
+            softWrap: true,
+          ),
+        ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              MySignInUpButton(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  text: 'Cancel'),
+              MySignInUpButton(
+                  onTap: () async {
+                    await Friends().deleteFriend(name, email);
+                    Navigator.pop(context);
+                  },
+                  text: 'OK'),
+            ],
+          )
+        ],
+      );
+    },
+  );
+}
+
+// Center(
+//         child: FloatingActionButton(
+//           child: const Text('Logout'),
+//           onPressed: (){
+//             AuthService().signOut();
+//           }
+//           ),
+//       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,161 +1,26 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
+import 'package:iba_course_2/authentication/auth_gate.dart';
+import 'package:iba_course_2/firebase_options.dart';
+import 'package:flutter/material.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Login Page',
-      home: LoginPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/authGate',
+      routes: {
+        '/authGate': (context) =>
+            const AuthGate(), //in lib>authentication>services
+      },
     );
-  }
-}
-
-class AuthService {
-  Future<String?> registration({
-    required String email,
-    required String password,
-  }) async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return 'Registration Successful';
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        return 'The password provided is too weak.';
-      } else if (e.code == 'email-already-in-use') {
-        return 'The account already exists for that email.';
-      } else {
-        return e.message;
-      }
-    } catch (e) {
-      return e.toString();
-    }
-  }
-
-  Future<String?> login({
-    required String email,
-    required String password,
-  }) async {
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return 'Login Successful';
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        return 'No user found for that email.';
-      } else if (e.code == 'wrong-password') {
-        return 'Wrong password provided for that user.';
-      } else {
-        return e.message;
-      }
-    } catch (e) {
-      return e.toString();
-    }
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final AuthService _authService = AuthService();
-  String? _message;
-  Color _messageColor = Colors.red;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 16),
-            if (_message != null) ...[
-              Text(
-                _message!,
-                style: TextStyle(
-                  color: _messageColor,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<void> _login() async {
-    final email = _emailController.text;
-    final password = _passwordController.text;
-
-    if (email.isEmpty || password.isEmpty) {
-      setState(() {
-        _message = 'Please fill in all fields.';
-        _messageColor = Colors.red;
-      });
-      return;
-    }
-
-    final result = await _authService.login(email: email, password: password);
-    setState(() {
-      _message = result;
-      _messageColor = result == 'Login Successful' ? Colors.green : Colors.red;
-    });
-  }
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
   }
 }
