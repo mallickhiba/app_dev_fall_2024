c4536a2fb1079a4ed856b926319aee4cf968514c
Hiba <mallickhiba@gmail.com>
2024-12-28 12:28:21 +0500

    q2 done

--- lib/weekly_expense.dart
+++ lib/weekly_expense.dart
@@ -6,9 +6,6 @@ class WeeklyExpensePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weekly Expense'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -24,13 +21,251 @@ class WeeklyExpensePage extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             Align(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
+              alignment: Alignment.topRight,
+              child: OutlinedButton(
                 onPressed: () {},
-                child: const Text('View Report'),
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+                child: const Text(
+                  'View Report',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
             ),
             const SizedBox(height: 16),
+            Expanded(
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    //purple 48
+                    Positioned(
+                      top: 40,
+                      left: 20,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 220,
+                            height: 220,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.purple.withOpacity(0.3),
+                            ),
+                          ),
+                          const Text(
+                            '48%',
+                            style: TextStyle(
+                              fontSize: 48,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // green 32
+                    Positioned(
+                      top: 20,
+                      right: 50,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 180,
+                            height: 180,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.green.withOpacity(0.3),
+                            ),
+                          ),
+                          const Text(
+                            '32%',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // red  13
+                    Positioned(
+                      bottom: 90,
+                      left: 160,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 140,
+                            height: 140,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.red.withOpacity(0.3),
+                            ),
+                          ),
+                          const Text(
+                            '13%',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // yeellow 7
+                    Positioned(
+                      bottom: 185,
+                      right: 100,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 80,
+                            height: 80,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.orange.withOpacity(0.3),
+                            ),
+                          ),
+                          const Text(
+                            '7%',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.orange,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Grocery',
+                            style:
+                                TextStyle(fontSize: 14, color: Colors.purple)),
+                        SizedBox(width: 20),
+                        Text(
+                          'Rs. 758.20',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.green,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Food & Drink',
+                            style:
+                                TextStyle(fontSize: 14, color: Colors.green)),
+                        SizedBox(width: 20),
+                        Text(
+                          'Rs. 758.20',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.start, // Aligns text to the start
+                      children: [
+                        Text('Shopping',
+                            style: TextStyle(fontSize: 14, color: Colors.red)),
+                        SizedBox(width: 20),
+                        Text(
+                          'Rs. 758.20',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.orange,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    const Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.start, // Aligns text to the start
+                      children: [
+                        Text(
+                          'Transportation',
+                          style: TextStyle(fontSize: 14, color: Colors.orange),
+                        ),
+                        SizedBox(width: 20),
+                        Text(
+                          'Rs. 758.20',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
